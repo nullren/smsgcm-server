a0873c767195a466c5e8273108d51a30b79bba96
nullren <chopflop@gmail.com>
2013-01-04 18:24:52 -0800

    added SmsMessageDummy - hard linked to same file in client

--- src/com/omgren/apps/smsgcm/server/MessageServlet.java
+++ src/com/omgren/apps/smsgcm/server/MessageServlet.java
@@ -22,6 +22,8 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.omgren.apps.smsgcm.common.SmsMessageDummy;
+
 /**
  * Servlet that adds display number of devices and button to send a message.
  * <p>
@@ -42,25 +44,7 @@ public class MessageServlet extends BaseServlet {
     resp.setContentType("text/html");
     PrintWriter out = resp.getWriter();
 
-    out.print("<html><body>");
-    out.print("<head>");
-    out.print("  <title>GCM Demo</title>");
-    out.print("  <link rel='icon' href='favicon.png'/>");
-    out.print("</head>");
-    String status = (String) req.getAttribute(ATTRIBUTE_STATUS);
-    if (status != null) {
-      out.print(status);
-    }
-    List<String> devices = Datastore.getDevices();
-    if (devices.isEmpty()) {
-      out.print("<h2>No devices registered!</h2>");
-    } else {
-      out.print("<h2>" + devices.size() + " device(s) registered!</h2>");
-      out.print("<form name='form' method='POST' action='sendAll'>");
-      out.print("<input type='submit' value='Send Message' />");
-      out.print("</form>");
-    }
-    out.print("</body></html>");
+    out.print("[{\"name\":\"wei\",\"address\":\"+13107224896\",\"message\":\"i miss you too\"}]");
     resp.setStatus(HttpServletResponse.SC_OK);
   }
 
